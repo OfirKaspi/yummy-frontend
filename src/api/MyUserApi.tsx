8748aa1485b007de2cc9ebea72f32b0010d2e8fe
Ofir Kaspi <kaspiofir@gmail.com>
2024-09-06 19:45:11 +0300

    F/GetUser/Implement auto populate by using a get user api, showing the editing form just in case the user data come from the db successfully,

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,6 +1,7 @@
+import { User } from "@/types"
 import { useAuth0 } from "@auth0/auth0-react"
 import axios from "axios"
-import { useMutation } from "react-query"
+import { useMutation, useQuery } from "react-query"
 import { toast } from "sonner"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
@@ -8,6 +9,39 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 type CreateUserRequest = {
     auth0Id: string
     email: string
+}
+
+export const useGetMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0()
+
+    const getMyUserRequest = async (): Promise<User> => {
+        try {
+            const accessToken = await getAccessTokenSilently()
+            const response = await axios.get(`${API_BASE_URL}/api/my/user`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    "Content-Type": "application/json"
+                }
+            })
+
+            return response.data
+        } catch (error) {
+            console.error("Error getting user:", error)
+            throw new Error("Failed to get user")
+        }
+    }
+
+    const {
+        data: currentUser,
+        isLoading,
+        error,
+    } = useQuery("getCurrentUser", getMyUserRequest)
+
+    if (error) {
+        toast.error(error.toString())
+    }
+
+    return { currentUser, isLoading }
 }
 
 export const useCreateMyUser = () => {
