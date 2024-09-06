e6a3266ec2169e577661b6d40664761b137d70a5
Ofir Kaspi <kaspiofir@gmail.com>
2024-09-06 18:23:30 +0300

    F/UpdateUser/Implement toast notifications using shadcn ui components

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './global.css'
 
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
+import { Toaster } from './components/ui/sonner'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
+          <Toaster visibleToasts={1} position='top-right' richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
