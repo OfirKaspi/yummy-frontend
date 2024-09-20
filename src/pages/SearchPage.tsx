58a3aecf3d3dbbcb16e200b47fd22ebd1d6d3b2e
Ofir Kaspi <kaspiofir@gmail.com>
2024-09-20 20:21:26 +0300

    F/Search/Made a new page and route for search results. Using zod for query validations and created the search bar himself which is located on the home page.

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+
+const SearchPage = () => {
+    const { city } = useParams()
+
+    return (
+        <div>User searched for {city}</div>
+    )
+}
+
+export default SearchPage
