9015e06288a4183a9d290f0654d1f5c3ce47f589
Ofir Kaspi <kaspiofir@gmail.com>
2024-09-21 12:46:29 +0300

    F/Search/Styled new components for the restaurant search results in a mobile first approach.

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,21 +1,33 @@
 import { useParams } from "react-router-dom"
 
 import { useSearchRestaurant } from "@/api/RestaurantApi"
+import LoadingButton from "@/components/LoadingButton"
+import SearchResultsInfo from "@/components/SearchResultsInfo"
+import SearchResultsCard from "@/components/SearchResultsCard"
 
 const SearchPage = () => {
     const { city } = useParams()
-    const { results } = useSearchRestaurant(city)
+    const { results, isLoading } = useSearchRestaurant(city)
+
+    if (isLoading) {
+        return <LoadingButton />
+    }
+
+    if (!results?.data || !city) {
+        return <span>No results found</span>
+    }
 
     return (
-        <div>
-            User searched for {city}{" "}
-            <span>
-                {results?.data.map((restaurant) => (
-                    <span>
-                        found - {restaurant.restaurantName}, {restaurant.city}
-                    </span>
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div id="cuisines-list">
+                insert cuisines here
+            </div>
+            <div id="main-content" className="flex flex-col gap-5">
+                <SearchResultsInfo total={results.pagination.total} city={city} />
+                {results.data.map((restaurant) => (
+                    <SearchResultsCard restaurant={restaurant} />
                 ))}
-            </span>
+            </div>
         </div>
     )
 }
