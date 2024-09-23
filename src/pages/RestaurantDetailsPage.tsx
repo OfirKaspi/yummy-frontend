05a402e34fce3be57d4219d1566810528f063f74
Ofir Kaspi <kaspiofir@gmail.com>
2024-09-23 19:41:35 +0300

    F/Cart/Create a cart for restaurant dishes, implemented the add functionality.

--- src/pages/RestaurantDetailsPage.tsx
+++ src/pages/RestaurantDetailsPage.tsx
@@ -1,13 +1,50 @@
+import { useParams } from "react-router-dom"
+
 import { useGetRestaurant } from "@/api/RestaurantApi"
 import MenuItem from "@/components/MenuItem"
 import RestaurantInfo from "@/components/RestaurantInfo"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import Loader from "@/components/ui/Loader"
-import { useParams } from "react-router-dom"
+import { useState } from "react"
+import { Card } from "@/components/ui/card"
+import OrderSummary from "@/components/OrderSummary"
+import { MenuItem as MenuItemType } from "@/types"
+
+export type CartItem = {
+    _id: string
+    name: string
+    price: number
+    quantity: number
+}
 
 const RestaurantDetailsPage = () => {
     const { restaurantId } = useParams()
     const { restaurant, isLoading } = useGetRestaurant(restaurantId)
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+    const addToCart = (menuItem: MenuItemType) => {
+        setCartItems((prevCartItems) => {
+            const existingCartItem = prevCartItems.find((cartItem) => cartItem._id === menuItem._id)
+            let updatedCartItems
+            if (existingCartItem) {
+                updatedCartItems = prevCartItems.map((cartItem) => cartItem._id === existingCartItem._id
+                    ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                    : cartItem
+                )
+            } else {
+                updatedCartItems = [
+                    ...prevCartItems, {
+                        _id: menuItem._id,
+                        name: menuItem.name,
+                        price: menuItem.price,
+                        quantity: 1,
+                    }
+                ]
+            }
+            return updatedCartItems
+        })
+    }
 
     if (isLoading || !restaurant) {
         return <Loader />
@@ -18,13 +55,18 @@ const RestaurantDetailsPage = () => {
             <AspectRatio ratio={16 / 5}>
                 <img src={restaurant?.imageUrl} className="rounded-md object-cover h-full w-full" />
             </AspectRatio>
-            <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+            <div className="grid md:grid-cols-[4fr_2fr] gap-5 lg:px-32">
                 <div className="flex flex-col gap-4">
                     <RestaurantInfo restaurant={restaurant} />
                     <span className="text-2xl font-bold tracking-tight">Menu</span>
                     {restaurant.menuItems.map((menuItem) => (
-                        <MenuItem menuItem={menuItem} />
+                        <MenuItem menuItem={menuItem} addToCart={() => addToCart(menuItem)} />
                     ))}
+                </div>
+                <div>
+                    <Card className="md:sticky md:top-5">
+                        <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+                    </Card>
                 </div>
             </div>
         </div>
