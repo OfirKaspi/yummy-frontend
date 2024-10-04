701c9025b8ce52e19eb573d428b197c0a5889600
Ofir Kaspi <kaspiofir@gmail.com>
2024-10-04 19:04:05 +0300

    F/UserOrder/Made a custom hook in order to get the orders and created an order type.

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -1,6 +1,7 @@
+import { Order } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
@@ -20,6 +21,32 @@ type CheckoutSessionRequest = {
     restaurantId: string
 }
 
+export const useGetMyOrders = () => {
+    const { getAccessTokenSilently } = useAuth0()
+
+    const getMyOrdersRequest = async (): Promise<Order[]> => {
+        try {
+            const accessToken = await getAccessTokenSilently()
+            const response = await axios.get(`${API_BASE_URL}/api/order`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+            return response.data
+        } catch (error) {
+            console.error("Error getting my orders:", error)
+            throw new Error("Failed to get my orders")
+        }
+    }
+
+    const { data: orders, isLoading } = useQuery(
+        "fetchMyOrders",
+        getMyOrdersRequest
+    )
+
+    return { orders, isLoading }
+}
+
 export const useCreateCheckoutSession = () => {
     const { getAccessTokenSilently } = useAuth0()
 
@@ -28,13 +55,11 @@ export const useCreateCheckoutSession = () => {
             const accessToken = await getAccessTokenSilently()
             const response = await axios.post(
                 `${API_BASE_URL}/api/order/checkout/create-checkout-session`,
-                checkoutSessionRequest,
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    },
-                }
-            )
+                checkoutSessionRequest, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+            })
             return response.data
         } catch (error) {
             console.error("Error creating order:", error)
--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,60 @@
 export type User = {
-    _id: string,
-    email: string,
+    _id: string
+    email: string
     name: string
-    addressLine1: string,
-    city: string,
-    country: string,
+    addressLine1: string
+    city: string
+    country: string
 }
 
 export type MenuItem = {
-    _id: string,
-    name: string,
-    price: number,
+    _id: string
+    name: string
+    price: number
 }
 
 export type Restaurant = {
-    _id: string,
-    user: string,
+    _id: string
+    user: string
     restaurantName: string
-    city: string,
-    country: string,
-    deliveryPrice: number,
-    estimatedDeliveryTime: number,
-    cuisines: string[],
-    menuItems: MenuItem[],
-    imageUrl: string,
-    lastUpdated: string,
+    city: string
+    country: string
+    deliveryPrice: number
+    estimatedDeliveryTime: number
+    cuisines: string[]
+    menuItems: MenuItem[]
+    imageUrl: string
+    lastUpdated: string
+}
+
+export type OrderStatus =
+    | "placed"
+    | "paid"
+    | "inProgress"
+    | "outForDelivery"
+    | "delivered"
+
+
+export type Order = {
+    _id: string
+    restaurant: Restaurant
+    user: User
+    deliveryDetails: {
+        email: string
+        name: string
+        addressLine1: string
+        city: string
+        country: string
+    }
+    cartItems: {
+        menuItemId: string
+        quantity: string
+        name: string
+    }[]
+    totalAmount: number
+    status: OrderStatus
+    createdAt: string
+    restaurantId: string
 }
 
 export type RestaurantSearchResponse = {
