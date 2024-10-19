b437409b3d2318410b8a367249ab2877afc54adb
Ofir Kaspi <kaspiofir@gmail.com>
2024-10-19 19:46:31 +0300

    F/FigmaDesign/New UI for the Loader.

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import { Loader2 } from 'lucide-react'
+import Yummy from "../../public/yummy.png"
 
 type Props = {
     isFullScreen?: boolean
@@ -7,7 +7,7 @@ type Props = {
 const Loader = ({ isFullScreen = false }: Props) => {
     return (
         <div className={`flex flex-col items-center justify-center ${isFullScreen && "h-screen"} space-y-4`}>
-            <Loader2 className="h-20 w-20 animate-spin text-orange-500" />
+            <img src={Yummy} alt="yummy-logo" className="h-20 w-20 animate-bounce" />
             <span className="text-lg font-semibold text-gray-700">Loading...</span>
         </div>
     )
