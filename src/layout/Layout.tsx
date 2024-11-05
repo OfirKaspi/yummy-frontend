ca0443c7258a47dee53d2574c0f29e2f2086553a
Ofir Kaspi <kaspiofir@gmail.com>
2024-11-05 19:46:55 +0200

    F/FigmaDesign/Improved desktop layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,9 +6,11 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
     return (
-        <div className="p-5 space-y-5 relative">
+        <div className="p-5 space-y-5 relative ">
             <MainNav />
-            {children}
+            <main className="sm:max-w-4xl sm:mx-auto">
+                {children}
+            </main>
         </div>
     )
 }
